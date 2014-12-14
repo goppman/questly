a997d9659e523ef4ce61a5ae6172a197a2800db1
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-13 20:44:29 -0500

    write some functions

--- js/questly.js
+++ js/questly.js
@@ -1,22 +1,20 @@
-//var name = prompt('What is your name?');
-//document.writeln('<p>Hi there, '+ name +'! </p>');
+var myForm, nameField, questField, userData;
+myForm = document.querySelector('form');
+nameField = myForm.elements.name;
+questField = myForm.elements.quest;
 
-
-var name = prompt('What is your name?');
-if (!name || name === 'null') {
-  name = 'Stupidhead';
+function getValues(ev) {
+  ev.preventDefault();
+  userData = {
+    name: nameField.value,
+    quest: questField.value
+  }
+  writeData();
 }
 
-var quest = prompt('What is your quest?');
-if (!quest || quest === 'null') {
-  quest = 'To become more cooperative';
+function writeData() {
+  var storyDiv = document.getElementById('story');
+  storyDiv.innerHTML = userData.name + ', ' + userData.quest;
 }
 
-var color = prompt('What is your favorite color?')
-if (!color || color === 'null') {
-  color = 'clear';
-}
-
-document.writeln('<p>There once was a warrior named ' + name +
-' on a mighty quest indeed: ' + quest + '. Luckily, ' + name +
-' was wearing lucky socks&mdash;colored ' + color + '</p>');
+myForm.onsubmit = getValues;
