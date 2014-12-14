cffabeb34bb255eba17c4c70ac608142a374f27d
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-14 11:26:26 -0500

    added a check box to questly

--- js/questly.js
+++ js/questly.js
@@ -1,4 +1,6 @@
-var myForm, nameField, questField, userData, colorField;
+function questly() {
+
+var myForm, nameField, questField, userData, colorField ;
 myForm = document.querySelector('form');
 nameField = myForm.elements.name;
 questField = myForm.elements.quest;
@@ -6,18 +8,32 @@ colorField = myForm.elements.color;
 
 function getValues(ev) {
   ev.preventDefault();
+
   userData = {
     name: nameField.value,
     quest: questField.value,
-    color: colorField.value
+    color: colorField.value,
+    person: this.elements.person.checked
   }
+
   writeData();
 }
 
 function writeData() {
   var storyDiv = document.getElementById('story');
-  storyDiv.innerHTML = userData.name + ', ' + userData.quest + ', ' +
+  var personStory = '';
+  if (userData.person){
+
+    personStory = 'You just brought a friend with you!';
+  }
+
+  storyDiv.innerHTML = userData.name + ', ' + userData.quest + ', '  + personStory + ' ' +
   userData.color;
 }
 
 myForm.onsubmit = getValues;
+
+
+}
+
+questly();
