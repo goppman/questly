1543f23704c608d9bb038105f0ac86678e3796b7
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-13 20:45:52 -0500

    Track experiments

---
+++ js/experiments.js
@@ -0,0 +1,18 @@
+var myForm = document.querySelector('form');
+var myStr1 = myForm.elements.str1;
+var myStr2 = myForm.elements.str2;
+
+function refreshFormValues () {
+  myStr1= myform.elements.str1.value;
+  myStr2= myform.elements.str2.value;
+
+
+}
+
+
+function concat (str1, str2) {
+
+  return str1+ ' ' + str2;
+
+
+}
--- js/prompts.js
+++ js/prompts.js
@@ -1,22 +1,2 @@
 //var name = prompt('What is your name?');
 //document.writeln('<p>Hi there, '+ name +'! </p>');
-
-
-var name = prompt('What is your name?');
-if (!name || name === 'null') {
-  name = 'Stupidhead';
-}
-
-var quest = prompt('What is your quest?');
-if (!quest || quest === 'null') {
-  quest = 'To become more cooperative';
-}
-
-var color = prompt('What is your favorite color?')
-if (!color || color === 'null') {
-  color = 'clear';
-}
-
-document.writeln('<p>There once was a warrior named ' + name +
-' on a mighty quest indeed: ' + quest + '. Luckily, ' + name +
-' was wearing lucky socks&mdash;colored ' + color + '</p>');
