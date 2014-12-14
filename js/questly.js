1c3cd8f3f0d487546b3737a00f4979e527eeaa3f
Greg Oppman <Greg.Oppman@WUSJLLPK0V29X.AM.JLLNET.com>
2014-12-14 10:15:57 -0500

    change quest to a radio button

--- js/questly.js
+++ js/questly.js
@@ -1,20 +1,23 @@
-var myForm, nameField, questField, userData;
+var myForm, nameField, questField, userData, colorField;
 myForm = document.querySelector('form');
 nameField = myForm.elements.name;
 questField = myForm.elements.quest;
+colorField = myForm.elements.color;
 
 function getValues(ev) {
   ev.preventDefault();
   userData = {
     name: nameField.value,
-    quest: questField.value
+    quest: questField.value,
+    color: colorField.value
   }
   writeData();
 }
 
 function writeData() {
   var storyDiv = document.getElementById('story');
-  storyDiv.innerHTML = userData.name + ', ' + userData.quest;
+  storyDiv.innerHTML = userData.name + ', ' + userData.quest + ', ' +
+  userData.color;
 }
 
 myForm.onsubmit = getValues;
